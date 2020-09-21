f7743a28104766ad71cf59b648eaf2a28a169d49
Arjun kumar verma <arjun.kumar.verma@jci.com>
2020-09-21 23:13:52 +0530

    Add basic exposed method

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  hubConnection: HubConnection;
+
+  constructor() {
+    // create connection with Hub
+    this.hubConnection = new HubConnectionBuilder().withUrl('http://localhost/signalr.API/signalrtc').build();
+
+    // register methods from server
+    this.hubConnection.on('newMessage', (data) => {
+      console.log(`newMessage: ${JSON.stringify(data)}`);
+    });
+    this.hubConnection.on('Send', (data) => {
+      console.log(`Send: ${JSON.stringify(data)}`);
+    });
+
+    // start connection with message hub.
+    this.hubConnection.start().then(() => {
+      console.log('connection established with message hub');
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  sendMessage = message => {
+    this.hubConnection.invoke('SendMessage', message);
+  }
+
+  sendMessageToGroup = (username, groupName, message) => {
+    this.hubConnection.invoke('SendMessageToGroup', username, groupName, message);
+  }
+
+  addToGroup = (username, groupName) => {
+    this.hubConnection.invoke('AddToGroup', username, groupName);
+  }
+
+  removeFromGroup = (username, groupName) => {
+    this.hubConnection.invoke('RemoveFromGroup', username, groupName);
+  }
 }
