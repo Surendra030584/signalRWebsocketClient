37f0de87d32b90084709452e5beb8baa8cc3b535
Arjun kumar verma <arjun.kumar.verma@jci.com>
2020-09-23 23:03:28 +0530

    User Websocket implementation in the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,39 @@ import { ChatService } from './chat.service';
 })
 export class AppComponent implements OnInit{
   title = 'test-chat-app';
-  constructor(private chatService: ChatService) { }
+  name = '';
+  messages: Array<string>;
+  constructor(private chatService: ChatService) { 
+    this.messages = new Array<string>();
+  }
 
   ngOnInit(): void {
-    this.chatService.startConnection().then(() => {
-      this.chatService.sendMessage('Hello Arjun');
-    });
+    // this.chatService.startConnection().then(() => {
+    //   this.chatService.sendMessage('Hello Arjun');
+    // });
+  }
+
+  run = () => {
+    // alert('Hello');
+    const url = `ws://localhost/WebAPIDemo/WebSocketServer.ashx?chatName=${this.name}`;
+    const w = new WebSocket(url);
+    w.onopen = () => {
+      console.log('open web socket');
+      w.send('thanks');
+    };
+
+    w.onmessage = (e) => {
+      this.messages.push(e.data.toString());
+      console.log(e.data.toString());
+    };
+
+    w.onclose = () => {
+      console.log('closed');
+    };
+
+    w.onerror = () => {
+      console.log('error');
+    };
   }
 
 }
